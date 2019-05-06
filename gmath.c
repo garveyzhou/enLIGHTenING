8dbce645cc367b292fe0cf6baace15f873ab0721
gzhou2@stuy.edu <gzhou2@stuy.edu>
2019-05-05 23:09:39 -0400

    done

--- gmath.c
+++ gmath.c
@@ -24,27 +24,72 @@
 //lighting functions
 color get_lighting( double *normal, double *view, color alight, double light[2][3], double *areflect, double *dreflect, double *sreflect) {
   color i;
+  normalize(normal);
+  normalize(light[LOCATION]);
+  normalize(view);
+  color a = calculate_ambient(alight,areflect);
+  color d = calculate_diffuse(light,dreflect,normal);
+  color s = calculate_specular(light,sreflect,view,normal);
+  i.red = a.red + d.red+s.red;
+  i.blue = a.blue + d.blue + s.blue ;
+  i.green = a.green + d.green + s.green ;
+  limit_color(&i);
+
   return i;
 }
 
 color calculate_ambient(color alight, double *areflect ) {
   color a;
+  a.red = alight.red * (areflect[RED]);
+  a.blue = alight.blue * (areflect[BLUE]);
+  a.green = alight.green * (areflect[GREEN]);
   return a;
 }
 
 color calculate_diffuse(double light[2][3], double *dreflect, double *normal ) {
   color d;
+  double diffuse =  dot_product(normal,light[LOCATION]);
+  if(diffuse < 0){
+    diffuse = 0;
+  }
+  d.red = light[COLOR][RED] * dreflect[RED] * diffuse;
+  d.green = light[COLOR][GREEN] * dreflect[GREEN] * diffuse;
+  d.blue = light[COLOR][BLUE] * dreflect[BLUE] * diffuse;
   return d;
 }
 
 color calculate_specular(double light[2][3], double *sreflect, double *view, double *normal ) {
-
   color s;
+  double d;
+  double reflect[3];
+
+  d = dot_product(normal, light[LOCATION]);
+  for (int i = 0; i < 3; i++) {
+    reflect[i] = normal[i] * d - light[LOCATION][i];
+  }
+  d = dot_product(reflect, view);
+  if (d < 0)
+    d = 0;
+  d = pow(d, SPECULAR_EXP);
+
+  s.red = light[COLOR][RED] * sreflect[RED] * d;
+  s.green = light[COLOR][GREEN] * sreflect[GREEN] * d;
+  s.blue = light[COLOR][BLUE] * sreflect[BLUE] * d;
+
   return s;
 }
 
 //limit each component of c to a max of 255
 void limit_color( color * c ) {
+  if(c->red > 255){
+    c->red = 255;
+  }
+  if(c->blue > 255){
+    c->blue = 255;
+  }
+  if(c->green > 255){
+    c->green = 255;
+  }
 }
 
 //vector functions
